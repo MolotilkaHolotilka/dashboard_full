6bbdf2bf48b2f04f3ea251a201cdf833d23e9b47
ilya <ilya@MacBook-Air-ilya.local>
2026-02-15 10:15:00 +0300

    add feat project scaffold

---
+++ backend/src/main/java/ru/dashboardbattle/DashboardBattleApplication.java
@@ -0,0 +1,12 @@
+package ru.dashboardbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardBattleApplication { // Главный класс приложения, через него все ранится
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardBattleApplication.class, args);
+    }
+}
